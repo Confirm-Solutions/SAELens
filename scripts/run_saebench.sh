24201d4a49827b3fa0bf2099581fccacd5f59838
sidnb13 <sidnbaskaran@gmail.com>
2025-06-23 18:42:00 +0000

    build: update submodules and dependencies for improved setup

---
+++ scripts/run_saebench.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+shopt -s nullglob
+
+PATHS=(/workspace/SAELens/checkpoints/topk-sae-debug-resid-post-160m/topk_*/final_*)
+
+# for path in "${PATHS[@]}"; do
+#     echo "launching absorption for $path"
+#     hook_layer=$(python3 -c "import yaml; print(yaml.safe_load(open('$path/hydra_config.yaml'))['hook_layer'])")
+#     echo "hook_layer: $hook_layer"
+#     ray job submit --entrypoint-num-gpus=0.25 --no-wait -- python -m sae_bench.evals.absorption.main --local_sae_path="$path" \
+#         --sae_block_pattern="blocks.$hook_layer.hook_resid_post" \
+#         --model_name="pythia-160m-deduped" \
+#         --output_folder="$path/absorption_results"
+# done
+
+# run `vllm serve --model google/gemma-3-12b-it --port 8000` for the server
+
+export CUDA_VISIBLE_DEVICES=0
+
+for path in "${PATHS[@]}"; do
+    echo "launching autointerp for $path"
+    hook_layer=$(python3 -c "import yaml; print(yaml.safe_load(open('$path/hydra_config.yaml'))['hook_layer'])")
+    python -m sae_bench.evals.autointerp.main --local_sae_path="$path" \
+        --sae_block_pattern="blocks.$hook_layer.hook_resid_post" \
+        --model_name="pythia-160m-deduped" \
+        --output_folder="$path/autointerp_results" \
+        --local \
+        --llm_api_model="google/gemma-3-12b-it" \
+        --llm_batch_size=512 \
+        --llm_dtype="bfloat16"
+done
